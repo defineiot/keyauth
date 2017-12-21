392b6d8c0f0c41ecdfcd7339325de2ca550691d6
liushi <1928385951@qq.com>
2017-12-21 14:55:31 +0800

    feat(testutils): add test utils

--- pkg/testutils/assert/assert_error.go
+++ pkg/testutils/assert/assert_error.go
@@ -1,20 +1,40 @@
 package assert
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+	"runtime"
+	"path/filepath"
 )
 
 func ErrorIsNil(t *testing.T, err error) {
 	if err != nil {
-		t.Fatal(err.Error())
+		who := resourceCall()
+		t.Fatalf("%s %s", who, err.Error())
 	}
 }
 
 func Contain(t *testing.T, err error, subString string) {
+	who := resourceCall()
 	if err == nil {
-		t.Fatal("error is nil")
+		t.Fatalf("%s error is nil", who)
 	} else if !strings.Contains(err.Error(), subString) {
-		t.Fatalf("error %s have not contain %s", err.Error(), subString)
+		t.Fatalf("%s error %s have not contain %s", who, err.Error(), subString)
 	}
+}
+
+func Equal(t *testing.T, actual, expect string) {
+	if actual != expect {
+		who := resourceCall()
+		t.Fatalf("%s %s not equal %s", who, actual, expect)
+	}
+}
+
+func resourceCall() string {
+	_, filename, line, ok := runtime.Caller(2)
+	if !ok {
+		return ""
+	}
+	return fmt.Sprintf("%s:%d: ", filepath.Base(filename), line)
 }
