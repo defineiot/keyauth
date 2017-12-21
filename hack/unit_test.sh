9e9beb1cbd6e645fd663b4a10a540f9643b7348d
liushi <1928385951@qq.com>
2017-12-21 14:55:31 +0800

    fix(bug): fix unit test shell

--- hack/unit_test.sh
+++ hack/unit_test.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-TOP_DIR=$(dirname `pwd`)
-
 function find_unittest_package(){
-    OLD_DIR=$(pwd)
-    cd ${TOP_DIR}
     pkgs=$(find . -name *test.go | xargs dirname | uniq | xargs go list -e -f '{{.ImportPath}}')
-    cd ${OLD_DIR}
     echo ${pkgs}
 }
 function run_unit_test(){
