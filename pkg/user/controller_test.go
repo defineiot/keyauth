9d7b23244db7adf8b7df10cc9981fc319f41d931
yumaojun03 <719118794@qq.com>
2017-12-30 13:00:04 +0800

    test(user): add pkg user unit test

--- pkg/user/controller_test.go
+++ pkg/user/controller_test.go
@@ -1,1 +1,119 @@
 package user_test
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+
+	"openauth/pkg/user"
+	domainmock "openauth/store/domain/mock"
+	"openauth/store/project"
+	projectmock "openauth/store/project/mock"
+	userstr "openauth/store/user"
+	usermock "openauth/store/user/mock"
+)
+
+func NewUserController() *user.Controller {
+	log := logrus.New()
+
+	ds := new(domainmock.DomainStore)
+	us := new(usermock.UserStore)
+	ps := new(projectmock.ProjectStore)
+
+	mockp := project.Project{
+		ID:       "validated-id",
+		Name:     "validated-name",
+		Enabled:  true,
+		DomainID: "validated-domain-id",
+	}
+	ps.GetProjectFn = func(id string) (*project.Project, error) {
+		if id == "validated-project-id" {
+			return &mockp, nil
+		}
+		return nil, fmt.Errorf("project %s not found", id)
+	}
+	ps.CheckProjectIsExistByIDFn = func(id string) (bool, error) {
+		if id == "validated-project-id" {
+			return true, nil
+		}
+		return false, nil
+	}
+
+	ds.CheckDomainIsExistByIDFn = func(domainID string) (bool, error) {
+		if domainID == "validated-domain-id" {
+			return true, nil
+		}
+		return false, nil
+	}
+
+	mocku := userstr.User{
+		ID:               "validated-user-id",
+		Name:             "validated-name",
+		Enabled:          true,
+		ExpireActiveDays: 4096,
+		DomainID:         "validated-domain-id",
+		DefaultProjectID: "validated-project-id",
+		Password: &userstr.Password{
+			ID:       1,
+			Password: "validated-password",
+			ExpireAt: 4096,
+		},
+	}
+	us.CreateUserFn = func(domainID, name, password string, enabled bool, userExpires, passExpires int) (*userstr.User, error) {
+		u := userstr.User{
+			ID:               "validated-user-id",
+			Name:             name,
+			Enabled:          enabled,
+			ExpireActiveDays: int64(userExpires),
+			DomainID:         domainID,
+			Password: &userstr.Password{
+				ID:       1,
+				Password: password,
+				ExpireAt: int64(passExpires),
+			},
+		}
+		return &u, nil
+	}
+	us.GetUserByIDFn = func(userID string) (*userstr.User, error) {
+		if userID == "validated-user-id" {
+			return &mocku, nil
+		}
+		return nil, fmt.Errorf("user %s not found", userID)
+	}
+	us.ListUserFn = func(domainID string) ([]*userstr.User, error) {
+		if domainID == "validated-domain-id" {
+			return []*userstr.User{&mocku}, nil
+		}
+		return []*userstr.User{}, nil
+	}
+	us.SetDefaultProjectFn = func(userID, projectID string) error {
+		if userID == "validated-user-id" {
+			return nil
+		}
+		return fmt.Errorf("user %s not found", userID)
+	}
+	us.ListUserProjectsFn = func(userID string) ([]string, error) {
+		if userID == "validated-user-id" {
+			return []string{"validated-project-id"}, nil
+		}
+
+		return nil, fmt.Errorf("user %s not found", userID)
+	}
+	us.AddProjectsToUserFn = func(userID string, projectIDs ...string) error {
+		if userID == "validated-user-id" {
+			return nil
+		}
+
+		return fmt.Errorf("user %s not found", userID)
+	}
+
+	us.RemoveProjectsFromUserFn = func(userID string, projectIDs ...string) error {
+		if userID == "validated-user-id" {
+			return nil
+		}
+
+		return fmt.Errorf("user %s not found", userID)
+	}
+
+	return user.NewController(log, us, ds, ps)
+}
--- pkg/user/user_test.go
+++ pkg/user/user_test.go
@@ -1,1 +1,124 @@
 package user_test
+
+import (
+	"testing"
+)
+
+func TestCreateUser(t *testing.T) {
+	t.Run("OK", testCreateOK)
+	t.Run("DomainNotFound", testCreateDomainNotFound)
+}
+
+func testCreateOK(t *testing.T) {
+	usersvr := NewUserController()
+
+	_, err := usersvr.CreateUser("validated-domain-id", "validated-username", "validated-password", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testCreateDomainNotFound(t *testing.T) {
+	usersvr := NewUserController()
+
+	_, err := usersvr.CreateUser("invalidated-domain-id", "validated-username", "validated-password", "")
+	if err.Error() != "domain invalidated-domain-id not exist" {
+		t.Fatal("want domain not found")
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	t.Run("OK", testGetUserOK)
+	t.Run("NotFound", testGetUserNotFound)
+}
+
+func testGetUserOK(t *testing.T) {
+	usersvr := NewUserController()
+
+	_, err := usersvr.GetUser("validated-user-id")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testGetUserNotFound(t *testing.T) {
+	usersvr := NewUserController()
+
+	_, err := usersvr.GetUser("invalidated-user-id")
+	if err.Error() != "user invalidated-user-id not found" {
+		t.Fatal("want not found error")
+	}
+}
+
+func TestListUser(t *testing.T) {
+	t.Run("OK", testListUserOK)
+	t.Run("DomainNotFound", testListUserDomainNotFound)
+}
+
+func testListUserOK(t *testing.T) {
+	usersvr := NewUserController()
+
+	_, err := usersvr.ListUser("validated-domain-id")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testListUserDomainNotFound(t *testing.T) {
+	usersvr := NewUserController()
+
+	_, err := usersvr.ListUser("invalidated-domain-id")
+	if err.Error() != "domain invalidated-domain-id not exist" {
+		t.Fatal("want domain not found")
+	}
+}
+
+func TestSetUserDefaultProject(t *testing.T) {
+	t.Run("OK", testSetDPOK)
+	t.Run("ProjectNotFound", testSetProjectNotFound)
+}
+
+func testSetDPOK(t *testing.T) {
+	usersvr := NewUserController()
+
+	if err := usersvr.SetUserDefaultProject("validated-user-id", "validated-project-id"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testSetProjectNotFound(t *testing.T) {
+	usersvr := NewUserController()
+
+	if err := usersvr.SetUserDefaultProject("validated-user-id", "invalidated-project-id"); err.Error() != "project invalidated-project-id not exist" {
+		t.Fatal("want project not found")
+	}
+}
+
+func TestListUserProjects(t *testing.T) {
+	t.Run("OK", testListUPOK)
+}
+
+func testListUPOK(t *testing.T) {
+	usersvr := NewUserController()
+
+	_, err := usersvr.ListUserProjects("validated-user-id")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestAddProjectsToUser(t *testing.T) {
+	usersvr := NewUserController()
+
+	if err := usersvr.AddProjectsToUser("validated-user-id", "validated-project-id"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRemoveProjectsFromUser(t *testing.T) {
+	usersvr := NewUserController()
+
+	if err := usersvr.RemoveProjectsFromUser("validated-user-id", "validated-project-id"); err != nil {
+		t.Fatal(err)
+	}
+}
